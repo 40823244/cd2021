0b8714ce4eb9fb288c392d7c9d88aff51ce52992
40823244 <40823244@gm.nfu.edu.tw>
2021-03-04 17:40:59 +0800

    add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '我的倉儲： https://github.com/40823244/cd2021 \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '我的倉儲： https://github.com/40823244/cd2021 \n \n', 'tags': '', 'url': 'About.html'}, {'title': '分組專題', 'text': '\n', 'tags': '', 'url': '分組專題.html'}]};
